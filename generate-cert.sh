b12bcbba1d71e4a56b42940172af44533ea37734
Andrew Sorensen <andrew@andrewsorensen.net>
2017-09-23 22:58:08 -0700

    Update executable locations

--- generate-cert.sh
+++ generate-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 IP=$1
-openssl req -new -sha256 -key server.key -subj "/C=US/ST=WA/O=Raspberry Pi/CN=${IP}" -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=IP:${IP}")) -out server.cert -days 730 -x509
+/usr/bin/openssl req -new -sha256 -key server.key -subj "/C=US/ST=WA/O=Raspberry Pi/CN=${IP}" -extensions SAN -config <(/bin/cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=IP:${IP}")) -out server.cert -days 730 -x509
